6a883f251a5f55e230822b2e53f361dca0e3da00
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2020-06-27 00:48:15 -0400

    update Ellipse Fitting

--- fitting/Ellipse Fitting.cpp
+++ fitting/Ellipse Fitting.cpp
@@ -1,9 +1,9 @@
 // Fitting ellipse to point set experiment (incomplete)
 
 /* To-do:
-	 - try to make it not fit to hyperbolas (test all eigenvalues)
-	 - handle the case when the matrix is not invertible
 	 - implement orthogonal least square fitting
+	 - try other eigenvalue algorithms
+	 - handle the case when the matrix is not invertible
 */
 
 
@@ -120,6 +120,15 @@ void matmul(const double A[6][6], const double x[6], double b[6]) {
 	}
 }
 
+// evaluate uᵀAv
+double quamul(const double u[6], const double A[6][6], const double v[6]) {
+	double s = 0;
+	for (int i = 0; i < 6; i++) for (int j = 0; j < 6; j++) {
+		s += u[i] * A[i][j] * v[j];
+	}
+	return s;
+}
+
 // trace of a matrix
 double trace(const double M[6][6]) {
 	double res = 0;
@@ -170,34 +179,112 @@ void matinv(const double M[6][6], double I[6][6]) {
 
 #pragma region Eigens
 
-// find the smallest eigenvalue of the matrix by solving its characteristic equation
-double EigenValue0_expand(const double M[6][6]) {
-	// expanding characteristic polynomial, works in O(n^4)
+// find the all eigenpairs of a matrix by solving its characteristic equation
+void EigenPairs_expand(const double M[6][6], double eigv[6], double eigvec[6][6]) {
+	double A[6][6]; matcpy(M, A);
+	double msc = 1.0;
+#if 1
+	// avoid overflow/precison error
+	msc = 1. / pow(determinant(A), 1. / 6);
+	if (0.0*msc == 0.0) {
+		for (int i = 0; i < 6; i++) for (int j = 0; j < 6; j++) A[i][j] *= msc;
+		msc = 1. / msc;
+	}
+	else msc = 1.0;
+#endif
+
+	// expand characteristic polynomial, works in O(n^4)
 	// method discribed at https://mathworld.wolfram.com/CharacteristicPolynomial.html
-	double C[7], Ci = -1;
-	double B[6][6]; matcpy(M, B);
-	C[0] = 1;
+	double C[7], C0[7], Ci = -1;
+	double B[6][6]; matcpy(A, B);
+	C0[0] = C[0] = 1;
 	for (int i = 1; i <= 6; i++) {
 		Ci = trace(B) / i;
 		double T[6][6]; matcpy(B, T);
 		for (int k = 0; k < 6; k++) T[k][k] -= Ci;
-		matmul(M, T, B);
-		C[i] = -Ci;
+		matmul(A, T, B);
+		C0[i] = C[i] = -Ci;
 	}
 	//printPolynomial(C, 6);
-	// Newton's iteration method starting at x=0
-	double x = 0;
-	for (int i = 0; i < 64; i++) {
-		double y = 0, dy = 0;
-		for (int i = 0; i <= 6; i++) {
-			y = y * x + C[i];
-			if (6 - i) dy = dy * x + (6 - i)*C[i];
+	matcpy(A, B);
+
+	// find the roots of the characteristic polynomial
+	for (int R = 6; R > 0; R--) {
+		// Newton's iteration method starting at x=0
+		// this should success because the matrix is positive difinite
+		double x = 0;
+		for (int i = 0; i < 64; i++) {
+			double y = 0, dy = 0;
+			for (int i = 0; i <= R; i++) {
+				y = y * x + C[i];
+				if (R - i) dy = dy * x + (R - i)*C[i];
+			}
+			double dx = y / dy;
+			x -= dx;
+			if (dx*dx < 1e-24) break;
 		}
-		double dx = y / dy;
-		x -= dx;
-		if (dx*dx < 1e-24) break;
+#if 0
+		// "refine" the root using the original polynomial
+		for (int i = 0; i < 3; i++) {
+			double y = 0, dy = 0;
+			for (int i = 0; i <= 6; i++) {
+				y = y * x + C0[i];
+				if (6 - i) dy = dy * x + (6 - i)*C0[i];
+			}
+			double dx = y / dy;
+			x -= dx;
+			if (dx*dx < 1e-24) break;
+		}
+#endif
+		// divide the root from the polynomial
+		for (int i = 0; i < R; i++) {
+			C[i + 1] += C[i] * x;
+		}
+		// export the eigenvalue
+		eigv[6 - R] = x * msc;
+
+		// find the eigenvector from the eigenvalue
+		double v[6];
+		matcpy(B, A);
+		for (int i = 0; i < 6; i++) A[i][i] -= x;
+		int sp = -1;
+		const double eps = 1e-6;
+		for (int i = 0, d = 0; d < 6; i++, d++) {
+			if (abs(A[i][d]) < eps) {
+				for (int j = i + 1; j < 6; j++) {
+					if (!(abs(A[j][d]) < eps)) {
+						for (int k = d; k < 6; k++) std::swap(A[i][k], A[j][k]);
+						break;
+					}
+				}
+			}
+			if (abs(A[i][d]) < eps) {
+				sp = d;
+				i--; continue;
+			}
+			else if (i == 5) {  // idk why this happens
+				sp = 5;
+				break;
+			}
+			double m = 1. / A[i][d];
+			for (int k = d; k < 6; k++) A[i][k] *= m;
+			for (int j = 0; j < 6; j++) if (j != i) {
+				double m = A[j][d];
+				for (int k = d; k < 6; k++) A[j][k] -= m * A[i][k];
+			}
+		}
+		for (int i = 0; i < sp; i++) v[i] = -A[i][sp];
+		v[sp] = 1;
+		for (int i = sp + 1; i < 6; i++) v[i] = 0;
+
+		// try to "refine" the eigenvector using some iterative methods
+
+		// export normalized eigenvector
+		double m = 1;
+		for (int i = 0; i < sp; i++) m += v[i] * v[i];
+		m = 1. / sqrt(m);
+		for (int i = 0; i < 6; i++) eigvec[6 - R][i] = v[i] * m;
 	}
-	return x;
 }
 
 // find an eigenpair using power iteration and inverse iteration
@@ -205,7 +292,8 @@ void EigenPair_powIter(const double M[6][6], double &u, double a[6]) {
 	for (int i = 0; i < 6; i++) a[i] = sqrt(1. / 6);
 	double A[6][6]; matcpy(M, A);
 #if 1
-	double m = 1. / pow(determinant(A), 1. / 6);  // should be non-negative
+	// avoid overflow/precison error
+	double m = 1. / pow(determinant(A), 1. / 6);
 	if (0.*m == 0.) {
 		for (int i = 0; i < 6; i++) for (int j = 0; j < 6; j++) A[i][j] *= m;
 		for (int i = 0; i < 2; i++) {
@@ -244,21 +332,43 @@ void EigenPair_invIter(const double M[6][6], double &u, double a[6]) {
 
 
 
-// ellipse fitting - v contains the coefficients of { x^2, xy, y^2, x, y, 1 }
+// ellipse fitting - v contains the coefficients of { x², xy, y², x, y, 1 }
 void fitEllipse(const vec2 *P, int N, double v[6]) {
-	// Minimize v*Mv, subject to v*v=1
-	// result may be hyperbola instead of ellipse
+	// Minimize vᵀMv, subject to vᵀv=1
 	double M[6][6]; generateMatrix(M, P, N);
 	double lambda; EigenPair_invIter(M, lambda, v);
+	// if not ellipse, check other eivenvalues
+	if (4.*v[0] * v[2] <= v[1] * v[1]) {
+		double eigv[6], eigvec[6][6];
+		EigenPairs_expand(M, eigv, eigvec);
+		for (int i = 1; i < 6; i++) {
+			auto w = eigvec[i];
+			if (4.*w[0] * w[2] > w[1] * w[1]) {
+				for (int j = 0; j < 6; j++) v[j] = w[j];
+				return;
+			}
+		}
+	}
 }
 void fitEllipse1(const vec2 *P, int N, double v[6]) {
+	// Minimize vᵀMv, subject to 4ac-b²=1
+	// I'm sure there is a bug because it sometimes get hyperbolas
 	double M[6][6]; generateMatrix(M, P, N);
 	const double F[6][6] = { {0,0,2,0,0,0},{0,-1,0,0,0,0},{2,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0} };
 	double I[6][6]; matinv(M, I);
 	double B[6][6]; matmul(I, F, B);
 	double u; EigenPair_powIter(B, u, v);
-	//printf("%lf\n", 4.*v[0] * v[2] - v[1] * v[1]);
-	u = 1. / u;
+	if (4.*v[0] * v[2] <= v[1] * v[1]) {
+		double eigv[6], eigvec[6][6];
+		EigenPairs_expand(B, eigv, eigvec);
+		for (int i = 1; i < 6; i++) {
+			auto w = eigvec[i];
+			if (4.*w[0] * w[2] > w[1] * w[1]) {
+				for (int j = 0; j < 6; j++) v[j] = w[j];
+				return;
+			}
+		}
+	}
 }
 
 
@@ -276,34 +386,10 @@ void fitEllipse1(const vec2 *P, int N, double v[6]) {
 // color structs
 typedef unsigned char byte;
 typedef struct { byte r, g, b; } COLOR;
-class COLORf {
-public:
-	double r, g, b;
-	COLORf() {}
-	COLORf(const double &r, const double &g, const double &b) :r(r), g(g), b(b) {}
-	COLORf(const COLOR &col) :r(col.r / 255.0), g(col.g / 255.0), b(col.b / 255.0) {}
-	COLORf operator - () const { return COLORf(-r, -g, -b); }
-	COLORf operator + (const COLORf &v) const { return COLORf(r + v.r, g + v.g, b + v.b); }
-	COLORf operator - (const COLORf &v) const { return COLORf(r - v.r, g - v.g, b - v.b); }
-	COLORf operator * (const COLORf &v) const { return COLORf(r * v.r, g * v.g, b * v.b); }
-	COLORf operator * (const double &k) const { return COLORf(k * r, k * g, k * b); }
-};
-COLOR toCOLOR(double c) {
-	byte k = (byte)(255.0*clamp(c, 0., 1.));
-	return COLOR{ k, k, k };
-}
-COLOR toCOLOR(double r, double g, double b) {
-	return COLOR{
-		(byte)(255.0*clamp(r, 0., 1.)),
-		(byte)(255.0*clamp(g, 0., 1.)),
-		(byte)(255.0*clamp(b, 0., 1.)),
-	};
-}
-COLOR toCOLOR(const COLORf &col) {
-	return toCOLOR(col.r, col.g, col.b);
-}
 COLOR mix(COLOR a, COLOR b, double d) {
-	return toCOLOR(COLORf(a)*(1.0 - d) + COLORf(b)*d);
+	auto f = [&](byte a, byte b) { return (byte)((1 - d)*a + d * b); };
+	//int k = d * 256; auto f = [&](byte a, byte b) { return (byte)(((256 - k)*a + k * b) >> 8); };
+	return COLOR{ f(a.r,b.r), f(a.g,b.g), f(a.b,b.b) };
 }
 
 // image variables
@@ -340,8 +426,7 @@ void drawDot(vec2 c, double r, COLOR col) {
 		for (int i = i0; i <= i1; i++) {
 			vec2 p = vec2(i - 0.5*W, 0.5*H - (j + 1)) * (1.0 / Scale) + Center;
 			double d = length(p - c) * Scale - r;
-			if (d < 0) canvas[j*W + i] = col;
-			else if (d < 1) canvas[j*W + i] = mix(col, canvas[j*W + i], d);  // shade by distance to anti-aliase
+			canvas[j*W + i] = mix(canvas[j*W + i], col, 0.75 * clamp(1. - d, 0., 1.));  // shade by distance to anti-aliase
 		}
 	}
 }
@@ -363,8 +448,7 @@ void drawQuadraticCurve(const double v[6], double width, COLOR col) {
 			double dy = buffer[j*W + i + W] - buffer[j*W + i - W];
 			double m = .5*sqrt(dx * dx + dy * dy);  // magnitude of gradient
 			double d = abs(buffer[j*W + i] / m) - r;  // divide by gradient to estimate distance
-			if (d < 0) canvas[j*W + i] = col;
-			else if (d < 1) canvas[j*W + i] = mix(col, canvas[j*W + i], d);
+			if ((d = 1. - d) > 0.) canvas[j*W + i] = mix(canvas[j*W + i], col, 0.8 * clamp(d, 0., 1.));
 		}
 	}
 }
@@ -387,11 +471,28 @@ bool save(const char* path) {
 // random tests for debug
 
 // test eigenvalue calculation
+bool checkEigenpair(const double M[6][6], double lambda, const double v[6]) {
+	bool ok = true;
+	double Ax[6]; matmul(M, v, Ax);
+	double m = 0; for (int i = 0; i < 6; i++) m += Ax[i] * Ax[i]; m = 1. / sqrt(m);
+	double e;
+	if ((e = abs(m * lambda - 1)) > 1e-6) {
+		printf("#%d %lg\n", __LINE__, e);
+		ok = false;
+	}
+	for (int i = 0; i < 6; i++) Ax[i] *= m;
+	m = 0; for (int i = 0; i < 6; i++) m += Ax[i] * v[i];
+	if ((e = abs(m - 1)) > 1e-6) {
+		printf("#%d %lg\n", __LINE__, e);
+		ok = false;
+	}
+	return ok;
+}
 void randomTest_eigen() {
 	auto t0 = std::chrono::high_resolution_clock::now();
 
 	double M[6][6];
-	for (int i = 0; i < 100000; i++)
+	for (int i = 0; i < 1000; i++)
 	{
 		_IDUM = i;
 		vec2 P[10];
@@ -399,32 +500,25 @@ void randomTest_eigen() {
 		generateMatrix(M, P, 10);
 		//printMatrix(M);
 
-		// calculate the smallest eigenvalue
-		double lambda, eigv[6];
-		double lambda0 = EigenValue0_expand(M);
+#if 0
+		double lambda, lambda0, eigv[6];
 		EigenPair_invIter(M, lambda, eigv);
-		double e = lambda - lambda0;  // Note: it is better to compare the value of the characteristic polynomial
-		if (e*e > 1e-12) {
-			printf("#%d %lg\n", __LINE__, e);
+		checkEigenpair(M, lambda, eigv);
+#else
+		double eigv[6], eigvec[6][6];
+		EigenPairs_expand(M, eigv, eigvec);
+		for (int t = 0; t < 6; t++) {
+			if (!checkEigenpair(M, eigv[t], eigvec[t])) {
+				printf("%d_%d\n", i, t);
+			}
 		}
-
-		// check
-		double Ax[6]; matmul(M, eigv, Ax);
-		double m = 0; for (int i = 0; i < 6; i++) m += Ax[i] * Ax[i]; m = 1. / sqrt(m);
-		if ((e = abs(m * lambda - 1)) > 1e-6) {
-			printf("#%d %lg\n", __LINE__, e);
-		}
-		for (int i = 0; i < 6; i++) Ax[i] *= m;
-		m = 0; for (int i = 0; i < 6; i++) m += Ax[i] * eigv[i];
-		if ((e = abs(m - 1)) > 1e-6) {
-			printf("#%d %lg\n", __LINE__, e);
-		}
+#endif
 	}
 
 	printf("%lfs elapsed\n", std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - t0).count());
 }
 
-// generate point data shaped like an ellipse
+// generate point data in the pattern of an ellipse
 void randomPointData(vec2 *P, int N) {
 	// parameters of the ellipse
 	double rx = abs(randf_n(1.) + 3.) + 1.;
@@ -486,7 +580,7 @@ void randomTest_image() {
 
 
 int main() {
-	randomTest_image();
+	randomTest_image(); exit(0);
 	return 0;
 }
 
