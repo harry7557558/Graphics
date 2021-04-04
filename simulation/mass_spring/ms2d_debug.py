4631cf0434afbee6906f5c3624d6d5988fc182d2
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2021-04-03 20:31:37 -0400

    get implicit integration work

--- simulation/mass_spring/ms2d_debug.py
+++ simulation/mass_spring/ms2d_debug.py
@@ -1,6 +1,7 @@
 # debug implicit integration for mass-spring simulation in 2d
 
 from math import *
+from copy import deepcopy
 import numpy as np
 import pygame
 
@@ -14,14 +15,15 @@ class vec2():
     def __neg__(self): return vec2(-self.x, -self.y)
     def __add__(self, other): return vec2(self.x+other.x, self.y+other.y)
     def __sub__(self, other): return vec2(self.x-other.x, self.y-other.y)
+    def sqr(self): return self.x*self.x + self.y*self.y
 
     def __mul__(self, other):
-        if type(other) == float:
+        if type(other) == float or type(other) == np.float64:
             return vec2(self.x*other, self.y*other)
         return vec2(self.x*other.x, self.y*other.y)
 
     def __truediv__(self, other):
-        if type(other) == float:
+        if type(other) == float or type(other) == np.float64:
             return vec2(self.x/other, self.y/other)
         return vec2(self.x/other.x, self.y/other.y)
 
@@ -63,8 +65,6 @@ class scene():
         spring_list = self.spring_list
         mn = len(mass_list)
         sn = len(spring_list)
-        for i in range(mn):
-            self.mass_list[i].f = vec2()
 
         # gravity
         for i in range(mn):
@@ -101,6 +101,124 @@ class scene():
         for i in range(len(self.mass_list)):
             mass_list[i].v += mass_list[i].f * mass_list[i].inv_m * dt
             mass_list[i].p += mass_list[i].v * dt
+
+        self.time += dt
+
+    def update_rungekutta(self, dt):
+        """ explicit Runge Kutta integration """
+
+        mn = len(self.mass_list)
+
+        # k1 = f(y0)
+        k1 = self
+        k1.calc_force()
+
+        # k2 = f(y0+h/2*k1)
+        k2 = deepcopy(self)
+        k2.time += 0.5*dt
+        for i in range(mn):
+            k2.mass_list[i].p += k1.mass_list[i].v * 0.5*dt
+            k1.mass_list[i].f *= k1.mass_list[i].inv_m
+            k2.mass_list[i].v += k1.mass_list[i].f * 0.5*dt
+        k2.calc_force()
+
+        # k3 = f(y0+h/2*k2)
+        k3 = deepcopy(self)
+        k3.time += 0.5*dt
+        for i in range(mn):
+            k3.mass_list[i].p += k2.mass_list[i].v * 0.5*dt
+            k2.mass_list[i].f *= k2.mass_list[i].inv_m
+            k3.mass_list[i].v += k2.mass_list[i].f * 0.5*dt
+        k3.calc_force()
+
+        # k4 = f(y0+h*k3)
+        k4 = deepcopy(self)
+        k4.time += dt
+        for i in range(mn):
+            k4.mass_list[i].p += k3.mass_list[i].v * dt
+            k3.mass_list[i].f *= k3.mass_list[i].inv_m
+            k4.mass_list[i].v += k3.mass_list[i].f * dt
+
+        # y1=y0+h/6*(k1+2*k2+2*k3+k4)
+        self.time += dt
+        for i in range(mn):
+            self.mass_list[i].p += (k1.mass_list[i].v + k2.mass_list[i].v*2.0 +
+                                    k3.mass_list[i].v*2.0 + k4.mass_list[i].v) * dt/6.0
+            self.mass_list[i].v += (k1.mass_list[i].f + k2.mass_list[i].f*2.0 +
+                                    k3.mass_list[i].f*2.0 + k4.mass_list[i].f) * dt/6.0
+
+    def update_impeuler(self, dt):
+        """
+            implicit integration: https://www.cs.cmu.edu/~baraff/papers/sig98.pdf
+            (I - h⋅M⁻¹⋅∂f/∂v - h²⋅M⁻¹⋅∂f/∂x) Δv = h⋅M⁻¹ ⋅ (f(x₀,v₀) + h⋅∂f/∂x⋅v₀),   Δx = h⋅(v₀+Δv)
+        """
+
+        mass_list = self.mass_list
+        mn = len(mass_list)
+
+        # calculate M⁻¹
+        inv_m = np.zeros((2*mn, 2*mn))
+        for i in range(mn):
+            inv_m[2*i][2*i] = inv_m[2*i+1][2*i+1] = mass_list[i].inv_m
+
+        # calculate f(x₀,v₀)
+        x0 = np.zeros((2*mn))
+        v0 = np.zeros((2*mn))
+        for i in range(mn):
+            x0[2*i] = mass_list[i].p.x
+            x0[2*i+1] = mass_list[i].p.y
+            v0[2*i] = mass_list[i].v.x
+            v0[2*i+1] = mass_list[i].v.y
+
+        def calc_force(x, v):
+            k = deepcopy(self)
+            for i in range(mn):
+                k.mass_list[i].p = vec2(x[2*i], x[2*i+1])
+                k.mass_list[i].v = vec2(v[2*i], v[2*i+1])
+            k.calc_force()
+            f = np.zeros((2*mn))
+            for i in range(mn):
+                f[2*i] = k.mass_list[i].f.x
+                f[2*i+1] = k.mass_list[i].f.y
+            return f
+
+        f0 = calc_force(x0, v0)
+
+        # calculate ∂f/∂x
+        eps = 0.0001
+        dfdx = np.zeros((2*mn, 2*mn))
+        for i in range(2*mn):
+            dx = np.zeros((2*mn))
+            dx[i] = eps
+            dfdx[i] = (calc_force(x0+dx, v0) -
+                       calc_force(x0-dx, v0)) / (2.0*eps)
+        dfdx = np.transpose(dfdx)
+
+        # calculate ∂f/∂v
+        dfdv = np.zeros((2*mn, 2*mn))
+        for i in range(2*mn):
+            dv = np.zeros((2*mn))
+            dv[i] = eps
+            dfdv[i] = (calc_force(x0, v0+dv) -
+                       calc_force(x0, v0-dv)) / (2.0*eps)
+        dfdv = np.transpose(dfdv)
+
+        # left of the linear system: I-h⋅M⁻¹⋅∂f/∂v-h²⋅M⁻¹⋅∂f/∂x
+        equ_left = np.identity(2*mn) - dt*np.matmul(inv_m, dfdv) \
+            - dt*dt*np.dot(inv_m, dfdx)
+
+        # right of the linear system: h⋅M⁻¹ ⋅ (f(x₀,v₀) + h⋅∂f/∂x⋅v₀)
+        equ_right = np.dot(dt*inv_m, f0 + dt*np.dot(dfdx, v0))
+
+        # calculate the change of velocity and position
+        dv = np.linalg.solve(equ_left, equ_right)
+        dx = dt*(v0+dv)
+
+        # update
+        self.time += dt
+        for i in range(mn):
+            mass_list[i].p += vec2(dx[2*i], dx[2*i+1])
+            mass_list[i].v += vec2(dv[2*i], dv[2*i+1])
 
 
 #
@@ -223,12 +341,17 @@ class PresetScenes():
 
 
 obj = PresetScenes().box_x()
-obj = PresetScenes().box_xx(5, 4, 1.0, 0.8, 0.2)
+obj = PresetScenes().box_xx(1, 1, 1.0, 1.0, 0.0)
+#obj = PresetScenes().box_xx(5, 4, 1.0, 0.8, 0.2)
 #obj = PresetScenes().box_xx(10, 8, 2.0, 1.6, 1.5)
 #obj = PresetScenes().polygon_s(8, 0.5, 0.8)
-#obj = PresetScenes().polygon_s(16, 0.5, 1.0)
+obj = PresetScenes().polygon_s(16, 0.5, 1.0)
 #obj = PresetScenes().sheet_hang(16, 8, 3.0, 1.5, False, False)
-obj_expeuler = obj
+obj = PresetScenes().sheet_hang(4, 2, 3.0, 1.5, False, False)
+
+obj_expeuler = deepcopy(obj)
+obj_rungekutta = deepcopy(obj)
+obj_impeuler = deepcopy(obj)
 
 
 #
@@ -252,25 +375,45 @@ def transform(p):
     return [xy.x, HEIGHT-xy.y]
 
 
-def draw_object(screen, obj):
+def draw_object(screen, obj, color):
     for s in obj.spring_list:
         i, j = s.m1, s.m2
         pygame.draw.line(screen, (255, 255, 255),
                          transform(obj.mass_list[i].p), transform(obj.mass_list[j].p), 1)
     for m in obj.mass_list:
-        pygame.draw.circle(screen, (255, 0, 0), transform(m.p), 2)
+        pygame.draw.circle(screen, color, transform(m.p), 2)
 
 
-while True:
+SHOW_REFERENCE = False
+CALC_ERROR = True
+err_obj_1 = obj_rungekutta
+err_obj_2 = obj_impeuler
+
+for frame in range(2**16):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
     # update objects
-    N = int(0.01 / 0.01)
-    dt = 0.01 / N
+    dt = 0.04
+    N = int(0.04 / dt)
+    dt = 0.5
     for i in range(N):
-        obj_expeuler.update_expeuler(dt)
+        # obj_expeuler.update_expeuler(dt)
+        obj_rungekutta.update_rungekutta(dt)
+        obj_impeuler.update_impeuler(dt)
+    if SHOW_REFERENCE:
+        dt = 0.001
+        N = int(0.04 / dt)
+        for i in range(N):
+            obj.update_rungekutta(dt)
+
+    # calculate the difference to see if the objects converge as time step decrease
+    if CALC_ERROR:
+        err = 0.0
+        for i in range(len(obj.mass_list)):
+            err += (err_obj_2.mass_list[i].p - err_obj_1.mass_list[i].p).sqr()
+        print("({},{:.6f})".format(frame+1, sqrt(err/len(obj.mass_list))), end=',')
 
     screen.fill((0, 0, 0))
 
@@ -285,9 +428,13 @@ while True:
                      transform(vec2(0, 0)), transform(vec2(0, 1)), 1)
 
     # draw objects
-    draw_object(screen, obj_expeuler)
+    if SHOW_REFERENCE:
+        draw_object(screen, obj, (128, 128, 128))
+    #draw_object(screen, obj_expeuler, (255, 0, 0))
+    draw_object(screen, obj_rungekutta, (0, 0, 255))
+    draw_object(screen, obj_impeuler, (255, 0, 255))
 
     pygame.display.flip()
-    pygame.time.Clock().tick(100)
+    pygame.time.Clock().tick(25)
 
 pygame.quit()
