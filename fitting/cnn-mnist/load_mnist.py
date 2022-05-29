0bcf602b41e38d4a26b8c6e8e97326a6c86cd73e
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2022-05-28 23:48:00 -0400

    convnet mnist

---
+++ fitting/cnn-mnist/load_mnist.py
@@ -0,0 +1,21 @@
+import numpy as np
+from keras.datasets import mnist
+
+(train_x, train_y), (test_x, test_y) = mnist.load_data()
+
+assert train_x.shape == (60000, 28, 28)
+assert test_x.shape == (10000, 28, 28)
+assert train_x.dtype == np.ubyte
+assert test_x.dtype == np.ubyte
+
+all_x = np.concatenate((train_x, test_x))
+all_y = np.concatenate((train_y, test_y))
+
+train_x.tofile("bin/train_x.bin")
+test_x.tofile("bin/test_x.bin")
+train_y.astype(np.uint8).tofile("bin/train_y.bin")
+test_y.astype(np.uint8).tofile("bin/test_y.bin")
+all_x.astype(np.uint8).tofile("bin/all_x.bin")
+all_y.astype(np.uint8).tofile("bin/all_y.bin")
+
+open("bin/.gitignore", 'w').write("*")
